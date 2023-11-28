51d38a6f3bd85967c87531a065dfa07d000eae3b
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2023-11-28 17:52:45 +0900

    Add files via upload

---
+++ src/main/java/user/UserDAO.java
@@ -0,0 +1,124 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class UserDAO {
+	
+	DataSource dataSource;
+	
+	public UserDAO() {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				if (rs.getString("userPassword").equals(userPassword)) {
+					return 1; // success
+				}
+				return 2; // password error
+			} else {
+				return 0; // not exist
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != rs) rs.close();
+				if (null != pstmt) pstmt.close();
+				if (null != conn) conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; //database error
+	}
+	
+	public int registerCheck(String userID) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next() || userID.equals("")) {
+				return 0; // duplication
+			} else {
+				return 1; // possible
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != rs) rs.close();
+				if (null != pstmt) pstmt.close();
+				if (null != conn) conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; //database error
+	}
+	
+	public int register(String userID, String userPassword, String userName, String userAge, String userGender, String userEmail, String userProfile) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.setString(2, userPassword);
+			pstmt.setString(3, userID);
+			pstmt.setInt(4, Integer.parseInt(userAge));
+			pstmt.setString(5, userGender);
+			pstmt.setString(6, userEmail);
+			pstmt.setString(7, userProfile);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next() || userID.equals("")) {
+				return 0; // duplication
+			} else {
+				return 1; // possible
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != rs) rs.close();
+				if (null != pstmt) pstmt.close();
+				if (null != conn) conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; //database error
+	}
+
+}
---
+++ src/main/java/user/UserDTO.java
@@ -0,0 +1,57 @@
+package user;
+
+public class UserDTO {
+	
+	String userID;
+	String userPassword;
+	String useruserName;
+	int useruserAge;
+	String userEmail;
+	String userGender;
+	String userProfile;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUseruserName() {
+		return useruserName;
+	}
+	public void setUseruserName(String useruserName) {
+		this.useruserName = useruserName;
+	}
+	public int getUseruserAge() {
+		return useruserAge;
+	}
+	public void setUseruserAge(int useruserAge) {
+		this.useruserAge = useruserAge;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserProfile() {
+		return userProfile;
+	}
+	public void setUserProfile(String userProfile) {
+		this.userProfile = userProfile;
+	}
+	
+	
+
+}
