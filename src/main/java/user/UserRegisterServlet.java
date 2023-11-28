b8f48724ca3e078f5efb5b1367a0795b39dcee01
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2023-11-28 22:53:17 +0900

    Add files via upload

---
+++ src/main/java/user/UserRegisterServlet.java
@@ -0,0 +1,54 @@
+package user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserRegisterServlet")
+public class UserRegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		String userID = request.getParameter("userID");
+		String userPassword1 = request.getParameter("userPassword1");
+		String userPassword2 = request.getParameter("userPassword2");
+		String userAge = request.getParameter("userAge");
+		String userName = request.getParameter("userName");
+		String userGender = request.getParameter("userGender");
+		String userEmail = request.getParameter("userEmail");
+		String userProfile = request.getParameter("userProfile");
+		if (null == userID || userID.equals("") || null == userPassword1 || userPassword1.equals("")
+				|| null == userPassword2 || userPassword2.equals("") || null == userAge || userAge.equals("")
+				|| null == userName || userName.equals("") || null == userGender || userGender.equals("")
+				|| null == userEmail || userEmail.equals("")) {
+			request.getSession().setAttribute("messageType", "오류 메세지");
+			request.getSession().setAttribute("messageContent", "입력되지 않은 항목이 있습니다");
+			response.sendRedirect("join.jsp");
+			return;
+		}
+		if (!userPassword1.equals(userPassword2)) {
+			request.getSession().setAttribute("messageType", "오류 메세지");
+			request.getSession().setAttribute("messageContent", "비밀번호가 서로 다릅니다");
+			response.sendRedirect("join.jsp");
+			return;
+		}
+		int result = new UserDAO().register(userID, userPassword1, userName, userAge, userGender, userEmail, userProfile);
+		if (1 == result) {
+			request.getSession().setAttribute("messageType", "성공 메세지");
+			request.getSession().setAttribute("messageContent", "회원가입이 완료되었습니다.");
+			response.sendRedirect("join.jsp");
+			return;
+		} else {
+			request.getSession().setAttribute("messageType", "오류 메세지");
+			request.getSession().setAttribute("messageContent", "이미 존재하는 회원입니다");
+			response.sendRedirect("join.jsp");
+			return;
+		}
+	}
+
+}
