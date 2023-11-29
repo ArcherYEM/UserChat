2199b35a2e49db78cb13fc3b0f6c2a3ed59cbab0
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2023-11-29 13:15:57 +0900

    Add files via upload

---
+++ src/main/java/chat/ChatDAO.java
@@ -0,0 +1,142 @@
+package chat;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class ChatDAO {
+	
+	DataSource dataSource;
+	
+	public ChatDAO() {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<ChatDTO> getChatListByID(String fromID, String toID, String chatID){
+		ArrayList<ChatDTO> chatList = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM chat WHERE ((fromID = ? AND toID = ?) OR (fromID = ? AND toID = ?)) AND chatID > ? ORDER BY chatTime";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, fromID);
+			pstmt.setString(2, toID);
+			pstmt.setString(3, toID);
+			pstmt.setString(4, fromID);
+			pstmt.setInt(5, Integer.parseInt(chatID));
+			rs = pstmt.executeQuery();
+			chatList = new ArrayList<ChatDTO>();
+			while (rs.next()) {
+				ChatDTO chat = new ChatDTO();
+				chat.setChatID(rs.getInt("chatID"));
+				chat.setFromID(rs.getString("fromID").replaceAll(" ", "&nbps;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				chat.setToID(rs.getString("toID").replaceAll(" ", "&nbps;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				chat.setChatContent(rs.getString("chatContent").replaceAll(" ", "&nbps;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				int chatTime = Integer.parseInt(rs.getString("chatTime").substring(11, 13));
+				String timeType = "오전";
+				if (chatTime >= 12) {
+					timeType = "오후";
+					chatTime -= 12;
+				}
+				chat.setChatTime(rs.getString("chatTime").substring(0, 11) + " " + timeType + " " + chatTime + ":" + rs.getString("chatTime").substring(14, 16) + "");
+				chatList.add(chat);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != rs) rs.close();
+				if (null != pstmt) pstmt.close();
+				if (null != conn) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return chatList;
+	}
+	
+	public ArrayList<ChatDTO> getChatListByResent(String fromID, String toID, int number){
+		ArrayList<ChatDTO> chatList = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM chat WHERE ((fromID = ? AND toID = ?) OR (fromID = ? AND toID = ?)) AND chatID > (SELECT MAX(chatID) - ? FROM CHAT) ORDER BY chatTime";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, fromID);
+			pstmt.setString(2, toID);
+			pstmt.setString(3, toID);
+			pstmt.setString(4, fromID);
+			pstmt.setInt(5, number);
+			rs = pstmt.executeQuery();
+			chatList = new ArrayList<ChatDTO>();
+			while (rs.next()) {
+				ChatDTO chat = new ChatDTO();
+				chat.setChatID(rs.getInt("chatID"));
+				chat.setFromID(rs.getString("fromID").replaceAll(" ", "&nbps;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				chat.setToID(rs.getString("toID").replaceAll(" ", "&nbps;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				chat.setChatContent(rs.getString("chatContent").replaceAll(" ", "&nbps;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				int chatTime = Integer.parseInt(rs.getString("chatTime").substring(11, 13));
+				String timeType = "오전";
+				if (chatTime >= 12) {
+					timeType = "오후";
+					chatTime -= 12;
+				}
+				chat.setChatTime(rs.getString("chatTime").substring(0, 11) + " " + timeType + " " + chatTime + ":" + rs.getString("chatTime").substring(14, 16) + "");
+				chatList.add(chat);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != rs) rs.close();
+				if (null != pstmt) pstmt.close();
+				if (null != conn) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return chatList;
+	}
+	
+	public int submit(String fromID, String toID, String chatContent){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "INSERT INTO chat VALUES (null, ?, ?, ?, now())";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, fromID);
+			pstmt.setString(2, toID);
+			pstmt.setString(3, chatContent);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != rs) rs.close();
+				if (null != pstmt) pstmt.close();
+				if (null != conn) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; // database error
+	}
+
+}
